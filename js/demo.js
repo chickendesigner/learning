78ebe785fb42653904006e3956c77c8d40622c30
Timur Zhilenkov <timur.zhilenkov@booking.com>
2018-06-25 17:25:54 +0200

    WIP

--- js/demo.js
+++ js/demo.js
@@ -1,19 +1,47 @@
 
+class Item {
+  constructor (el) {
+  }
+}
+
 
 class Overlay {
   constructor() {
     this.DOM = {el: document.querySelector(".overlay")};
-    this.DOM.reveal = this.DOM.el.querySelector(".overlay__reveal");
     this.DOM.items = this.DOM.el.querySelectorAll(".overlay__item");
   }
+  show (contentItem) {
+    console.log("this is a show method");
+    this.contentItem = contentItem;
+    this.revealItem(contentItem);
+  }
 
-  show(){
-    this.DOM.el.classList.add("Testing!");
-    console.log(this.DOM.el);
+  revealItem () {
+    this.contentItem.style.opacity = 1;
   }
 }
+
+class Grid {
+  constructor () {
+    this.myItem = document.querySelector(".grid__item_1");
+    this.myItem.addEventListener('click', (ev) => {
+      ev.preventDefault();
+      this.openItem();
+    });
+
+    this.overla
+  }
+
+  openItem (contentItem) {
+    console.log(contentItem);
+  }
+
+}
+
 
 const z = new Overlay();
 z.show();
 
+const v = new Grid();
+console.log(v.myItem);
 
